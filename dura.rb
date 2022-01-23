071016d0331e06ac4edc7087cd441962ec402fa5
MisileLab <misileminecord@gmail.com>
2022-01-23 14:58:57 +0900

    test

--- dura.rb
+++ dura.rb
@@ -1,9 +1,9 @@
 class Dura < Formula
     desc "You shouldn't ever lose your work if you're using Git"
     homepage "https://github.com/MisileLab/homebrew-dura"
-    url "https://github.com/tkellogg/dura"
+    url "https://github.com/MisileLab/homebrew-dura"
     version "0.1.0"
-    sha256 "f527fc7b185db41c39a82b6fd0edcd6c9961d6fc "
+    sha256 ""
     license "Apache License, Version 2.0"
   
     depends_on "rust" => :build
