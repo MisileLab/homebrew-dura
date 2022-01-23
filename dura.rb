9ff63f36ed0b687573f9a611d94b38f40ebc6f06
MisileLaboratory <misileminecord@gmail.com>
2022-01-23 14:30:25 +0900

    Create dura.rb

---
+++ dura.rb
@@ -0,0 +1,23 @@
+class Dura < Formula
+    desc "You shouldn't ever lose your work if you're using Git"
+    homepage "https://github.com/tkellogg/dura"
+    url "dura"
+    version "0.1.0"
+    sha256 ""
+    license "Apache License, Version 2.0"
+  
+    depends_on "rust" => :build
+    depends_on "git"
+  
+    def install
+      # ENV.deparallelize  # if your formula fails when building in parallel
+      system "git", "clone", "https://github.com/tkellogg/dura", "-b", "v0.1.0"
+      system "cd", "dura"
+      system "cargo", "install", "--path", "."
+    end
+  
+    test do
+      system "false"
+    end
+  end
+  
